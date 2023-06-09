0b2c73892f664717621ffd7f9867030868675fda
gourjoy <goursaha307@gmail.com>
2023-06-09 11:28:49 +0600

    user post method added

--- index.js
+++ index.js
@@ -24,6 +24,21 @@ async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
+
+		const userCollection = client.db('RhythmRoam').collection('users');
+
+		// add the user to the collection
+		app.post('/users', async (req, res) => {
+			const user = req.body;
+			const query = { email: user.email };
+			const existingUser = await userCollection.findOne(query);
+			if (existingUser) {
+				return res.send({ message: 'user already exists!' });
+			}
+			const result = await userCollection.insertOne(user);
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db('admin').command({ ping: 1 });
 		console.log('Pinged your deployment. You successfully connected to MongoDB!');
