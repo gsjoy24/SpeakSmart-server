12316c8194a57d5e383fae04b5729520ba7eef04
gourjoy <goursaha307@gmail.com>
2023-06-09 14:34:28 +0600

    generated jwt

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
-app.use(cors());
+const corsOptions = {
+	origin: '*',
+	credentials: true,
+	optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nbdk5o7.mongodb.net/?retryWrites=true&w=majority'`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,22 +26,34 @@ const client = new MongoClient(uri, {
 	}
 });
 
+app.listen(port, () => console.log('listening on port', port));
+app.get('/', (req, res) => {
+	res.send('server is running');
+});
+
 async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
+		const usersCollection = client.db('RhythmRoam').collection('users');
 
-		const userCollection = client.db('RhythmRoam').collection('users');
+		// generate JWT
+		app.post('/jwt', async (req, res) => {
+			const email = req.body;
+			const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+			res.send({ token });
+		});
 
-		// add the user to the collection
-		app.post('/users', async (req, res) => {
+		// save user information
+		app.put('/users/:email', async (req, res) => {
+			const email = req.params.email;
 			const user = req.body;
-			const query = { email: user.email };
-			const existingUser = await userCollection.findOne(query);
-			if (existingUser) {
-				return res.send({ message: 'user already exists!' });
-			}
-			const result = await userCollection.insertOne(user);
+			const query = { email };
+			const options = { upsert: true };
+			const updatedDoc = {
+				$set: user
+			};
+			const result = await usersCollection.updateOne(query, updatedDoc, options);
 			res.send(result);
 		});
 
@@ -44,12 +62,7 @@ async function run() {
 		console.log('Pinged your deployment. You successfully connected to MongoDB!');
 	} finally {
 		// Ensures that the client will close when you finish/error
-		await client.close();
+		// await client.close();
 	}
 }
 run().catch(console.dir);
-
-app.listen(port, () => console.log('listening on port', port));
-app.get('/', (req, res) => {
-	res.send('server is running');
-});
